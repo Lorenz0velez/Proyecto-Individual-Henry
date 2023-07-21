a6a9b952464fc4f6d65c536b6b37c3b2629a640c
Lorenz0velez <lorenzoveleznavarro@gmail.com>
2023-07-21 01:27:31 -0300

    preparando todo para subir 21JUL 1:07am

--- client/src/Components/LandingPage/LandingPage.js
+++ client/src/Components/LandingPage/LandingPage.js
@@ -1,18 +1,34 @@
+// import React from "react";
+// import './LandingPage.css';
+// import { Link } from "react-router-dom";
+
+// export default function LandingPage(){
+
+//     return(
+//         <div className="conteiner-landing">
+//             <div>
+//             <h1 className="welcomeH1">BIENVENIDO A MI PROYECTO INDIVIDUAL DE SOYHENRY!</h1>
+//             </div>
+
+//             <Link to={'/countries'} style={{ textDecoration: 'none' }} >
+//                 <a href="/" className="bn13">¡VAMOS!</a>
+//             </Link>
+//         </div>
+//     )
+// }
 import React from "react";
-import './LandingPage.css';
 import { Link } from "react-router-dom";
+import './LandingPage.css';
 
-export default function LandingPage(){
-
-    return(
-        <div className="conteiner-landing">
-            <div>
-            <h1 className="welcomeH1">BIENVENIDO A MI PROYECTO INDIVIDUAL DE SOYHENRY!</h1>
-            </div>
-
-            <Link to={'/countries'} style={{ textDecoration: 'none' }} >
-                <a href="/" className="bn13">¡VAMOS!</a>
-            </Link>
-        </div>
-    )
+export default function LandingPage() {
+  return (
+    <div className="container-landing">
+      <div>
+        <h1 className="welcomeH1">BIENVENIDO A MI PROYECTO INDIVIDUAL DE SOYHENRY!</h1>
+      </div>
+      <Link to={'/countries'} style={{ textDecoration: 'none' }} >
+        <a href="/" className="btn13">¡VAMOS!</a>
+      </Link>
+    </div>
+  );
 }
