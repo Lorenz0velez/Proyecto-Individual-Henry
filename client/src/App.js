f7832ae0e1fe50788ed1551fe281649b0054a676
Lorenz0velez <lorenzoveleznavarro@gmail.com>
2023-07-21 01:07:15 -0300

    preparando todo para subir 21JUL 1:07am

--- client/src/App.js
+++ client/src/App.js
@@ -10,17 +10,14 @@ import LandingPage from './Components/LandingPage/LandingPage';
 
 // axios.defaults.baseURL = "http://localhost:3001/" ; 
 axios.defaults.baseURL = "https://proyecto-individual-henry-production-b38e.up.railway.app/";
-// axios.defaults.baseURL = "https://proyecto-individual-henry-dusky.vercel.app" ;
 
 function App() {
   return (
     <div className="App">
-      {/* <Router> */}
-       <Route exact path="/" component={LandingPage} />      
+      <Route exact path="/" component={LandingPage} />      
       <Route exact path="/countries" component={Home} />      
       <Route path="/countries/:id" component={CardDetail} />      
       <Route path="/create_activity" component={CreateActivity} />     
-      {/* </Router>  */}
     </div>
   );
 }
